754e861708ecf4b9f861ab634926752576112fb7
kndrad <kon.now@protonmail.com>
2024-10-04 13:59:34 +0200

    modified import path

--- internal/screenshot/screenshot_test.go
+++ internal/screenshot/screenshot_test.go
@@ -7,7 +7,7 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/kndrad/reckon/internal/screenshot"
+	"github.com/kndrad/itcrack/internal/screenshot"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
