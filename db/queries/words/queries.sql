14495eedbbde73605895cd5d5d5cfd649f0191b0
kndrad <kon.now@protonmail.com>
2024-11-07 10:59:58 +0100

    deleted: removed unused queries

--- db/queries/words/queries.sql
+++ db/queries/words/queries.sql
@@ -12,16 +12,3 @@ VALUES ($1, CURRENT_TIMESTAMP) ON CONFLICT (value) DO NOTHING
 RETURNING id,
     value,
     created_at;
--- name: GetWordByValue :one
-SELECT id,
-    value,
-    created_at
-FROM words
-WHERE value = $1
-    AND deleted_at IS NULL
-LIMIT 1;
--- name: SoftDeleteWord :exec
-UPDATE words
-SET deleted_at = CURRENT_TIMESTAMP
-WHERE id = $1
-    AND deleted_at IS NULL;
