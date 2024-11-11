602539d1936dc23fc17a3febf7d066c56cdd4f48
kndrad <kon.now@protonmail.com>
2024-11-11 18:37:15 +0100

    added query which returns word and it's rank (rank is determined by word count in a db)

--- db/queries/words/queries.sql
+++ db/queries/words/queries.sql
@@ -8,11 +8,19 @@ LIMIT $1 OFFSET $2;
 INSERT INTO words (value, created_at)
 VALUES ($1, CURRENT_TIMESTAMP)
 RETURNING id, value, created_at;
--- name: GetWordFrequency :many
+-- name: GetWordsFrequencies :many
 SELECT words.value, count(*) AS word_count
 FROM words
 WHERE deleted_at IS NULL
 GROUP BY words.value
 ORDER BY word_count ASC
 LIMIT $1 OFFSET $2;
-
+-- name: GetWordsRank :many
+SELECT
+    words.value,
+    ROW_NUMBER() OVER (ORDER BY count(*) DESC) as rank
+FROM words
+WHERE deleted_at IS NULL
+GROUP BY words.value
+ORDER BY rank ASC
+LIMIT $1 OFFSET $2;
